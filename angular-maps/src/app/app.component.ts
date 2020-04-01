535fa98697a7d6453bfbe8967883aeee8faf0cb7
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-04-01 18:31:45 +0000

    icone fatte

--- angular-maps/src/app/app.component.ts
+++ angular-maps/src/app/app.component.ts
@@ -14,6 +14,12 @@ export class AppComponent {
   latC: number = 45.518039;
   lngC: number = 9.202651;
 
+  lngG: number = 9.1;
+  latG: number = 45.5;
+
+  latPalme: number= 45.42;
+  lngPalme: number= 9.1;
+
   colore: string = '';
   changeRed(){
     this.colore = 'red';
@@ -24,4 +30,36 @@ export class AppComponent {
   changeGreen(){
     this.colore = 'green';
   }
+
+  public gatto =  {
+    url:'./assets/img/cat_acrobat.ico',
+    scaledSize: {
+      width: 60,
+      height: 60
+    }
+  };
+
+  public stella =  {
+    url:'./assets/img/star.ico',
+    scaledSize: {
+      width: 60,
+      height: 60
+    }
+  };
+
+  public italia =  {
+    url:'./assets/img/italia.ico',
+    scaledSize: {
+      width: 50,
+      height: 50
+    }
+  };
+
+  public palme =  {
+    url:'./assets/img/palme.ico',
+    scaledSize: {
+      width: 50,
+      height: 50
+    }
+  };
 }
