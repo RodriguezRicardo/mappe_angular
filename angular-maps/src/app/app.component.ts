b14aad9e65a7e32f0c951fe2d6007213e7cff127
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-04-01 16:06:32 +0000

    fino ai cerchi

---
+++ angular-maps/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'server mappe';
+  //aggiungere latitudine e longitudine
+  lat: number = 45.506738;
+  lng: number = 9.190766;
+
+  latC: number = 45.518039;
+  lngC: number = 9.202651;
+
+}
